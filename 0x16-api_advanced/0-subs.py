d1cb00f32ebd43924fe3dda18ba13d9faf9614ab
Winfred <winfrednginakilonzo@gmail.com>
2023-10-10 15:11:14 +0300

    Set allow redirects to false

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ import sys
 def number_of_subscribers(subreddit):
     '''If not a valid subreddit, return 0'''
     url = ('https://www.reddit.com/r/{}/about.json'.format(subreddit))
-    response = requests.get(url)
+    response = requests.get(url, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
         return (data['data']['subscribers'])
